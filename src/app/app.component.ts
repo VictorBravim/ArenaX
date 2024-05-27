7a8bf3909ca168aa47c1307abe84ad28d304862f
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-27 15:59:58 -0300

    Mudança

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'arenax';
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,9 @@
+// src/app/nav/nav.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  templateUrl: './nav.component.html',
+})
+export class NavComponent {}
