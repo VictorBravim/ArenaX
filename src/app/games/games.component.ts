6eeab27d0ada794b76d101bb78d82058135295ff
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-28 15:17:34 -0300

    Mudança

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,99 +1,35 @@
-// src/app/games/games.component.ts
-import { Component, Inject, PLATFORM_ID, HostListener, ViewChild } from '@angular/core';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { SlickCarouselComponent, SlickCarouselModule } from 'ngx-slick-carousel';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [CommonModule, SlickCarouselModule],
+  imports: [CommonModule],
   templateUrl: './games.component.html',
-  styleUrls: [],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrls: ['./games.component.css']
 })
 export class GamesComponent {
-  isMobile = false;
-  backgroundImage = '';
-
-  constructor(@Inject(PLATFORM_ID) private platformId: any) {
-    if (isPlatformBrowser(this.platformId)) {
-      this.updateBackgroundImage();
-    }
-  }
-
-  get backgroundImageStyle() {
-    return isPlatformBrowser(this.platformId) ? { 'background-image': `url(${this.backgroundImage})` } : {};
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event) {
-    if (isPlatformBrowser(this.platformId)) {
-      this.updateBackgroundImage();
-    }
-  }
-
-  updateBackgroundImage() {
-    this.isMobile = window.innerWidth <= 768;
-    this.backgroundImage = this.isMobile ? 'assets/images/bg-mobile2.png' : 'assets/images/bg2.png';
-  }
-
-  @ViewChild('slickModal') slickModal: SlickCarouselComponent | null = null;
-
-  carros = [
-    {
-      nome: 'Audi A4 Comum',
-      imagem: 'assets/A4.png',
-      preco: 'R$ 1.200',
-    },
-    {
-      nome: 'Audi R8 Esportivo',
-      imagem: 'assets/R8.png',
-      preco: 'R$ 5.000',
-    },
-    {
-      nome: 'Audi Q7 Esportivo',
-      imagem: 'assets/Q7.png',
-      preco: 'R$ 4.000',
-    },
-    {
-      nome: 'Audi Q3 Comun',
-      imagem: 'assets/Q3.png',
-      preco: 'R$ 2.000',
-    },
-    {
-      nome: 'Audi TT Esportivo',
-      imagem: 'assets/TT.png',
-      preco: 'R$ 2.500',
-    },
+  games = [
+    { name: 'Game 1', image: 'assets/images/game1.jpg' },
+    { name: 'Game 2', image: 'assets/images/game2.jpg' },
+    { name: 'Game 3', image: 'assets/images/game3.jpg' },
   ];
 
-  sliderConfig = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    draggable: true,
-    swipeToSlide: true,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
+  currentIndex = 0;
 
-  nextSlide() {
-    this.slickModal?.slickNext();
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.games.length;
+    this.updateTransform();
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.games.length) % this.games.length;
+    this.updateTransform();
+  }
+
+  updateTransform() {
+    const carousel = document.querySelector('.carousel') as HTMLElement;
+    const offset = -this.currentIndex * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
   }
 }
